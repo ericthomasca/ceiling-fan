306261841fdf0b4df61a95e8bd66ed0f936f8a05
Eric Thomas <eric@ericthomas.ca>
2022-07-27 21:44:54 -0230

    java docs

--- src/CeilingFan.java
+++ src/CeilingFan.java
@@ -1,20 +1,38 @@
+/**
+ * Ceiling fan with 3 levels of speed and 2 directions.
+ * @author Eric Thomas
+ */
 public class CeilingFan {
     private static int speed = 0;
     private static String direction = "clockwise";
 
+    /**
+     * Get the speed of the ceiling fan.
+     * @return the speed of the ceiling fan.
+     */
     public static int getSpeed() {
         return speed;
     }
 
+    /**
+     * Get the direction of the ceiling fan.
+     * @return the direction of the ceiling fan.
+     */
     public static String getDirection() {
         return direction;
     }
 
+    /**
+     * Set the speed of the ceiling fan. Every pull increases the speed by 1 to a maximum of 3.
+     */
     public static void pullSpeedCord() {
         speed++;
         speed = speed % 4;
     }
 
+    /**
+     * Set the direction of the ceiling fan. Every pull reverses the direction.
+     */
     public static void pullDirectionCord() {
         if (direction.equals("clockwise")) {
             direction = "counterclockwise";
